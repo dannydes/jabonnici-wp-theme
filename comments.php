25179be35bfa817f11ff503ca03929286bb4d0fa
Daniel Desira <desradaniel2007@gmail.com>
2016-02-23 13:41:45 +0100

    better posts and comments

--- comments.php
+++ comments.php
@@ -1,51 +1,58 @@
 <div <?php comment_class(); ?>>
-<?php if ( have_comments() ): ?>
-	<span class="label label-default"><?php echo get_comments_number(); ?> comments</span>
+	<div class="well">
+	<?php if ( have_comments() ): ?>
+		<p><span class="label label-default"><?php echo get_comments_number(); ?> comments</span></p>
+		<?php
+		
+		wp_list_comments( array(
+			'avatar_size' => 40,
+			'per_page' => 5,
+			'short_ping' => TRUE,
+			'style' => 'div',
+		) );
+		
+		the_comments_navigation();
+		
+		?>
+	<?php endif; ?>
+	<?php if ( ! comments_open() ): ?>
+		<p><?php _e( 'Comments are closed.', 'jabonnici' ); ?></p>
+	<?php endif; ?>
 	<?php
+
+	$commenter = wp_get_current_commenter();
+	$req = get_option( 'require_name_email' );
+	$aria_req = ( $req ? ' aria-required' : '' );
 	
-	wp_list_comments( array(
-		'avatar_size' => 40,
-		'per_page' => 5,
-		'short_ping' => TRUE,
+	$fields = array(
+		'author' => '<div class="form-group comment-form-author">
+				<label for="author">' . __( 'Name', 'domainreference' ) . '</label> ' .
+				( $req ? '<span class="required label label-default">*</span>' : '' ) .
+				'<input class="form-control" id="author" name="author" type="text" value="' .
+				esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . '>
+			</div>',
+		'email' => '<div class="form-group comment-form-email"><label for="email">' . __( 'Email', 'domainreference' ) . '</label> ' .
+				( $req ? '<span class="required label label-default">*</span>' : '' ) . ' (' . __( 'Will be kept anonymous.' ) . ')
+				<input class="form-control" id="email" name="email" type="email" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+				'" size="30"' . $aria_req . ' />
+			</div>',
+		'url' => '<div class="form-group comment-form-url">
+					<label for="url">' . __( 'Website', 'domainreference' ) . '</label>' .
+					'<input class="form-control" id="url" name="url" type="url" value="' . esc_attr( $commenter['comment_author_url'] ) .
+					'" size="30" />
+			</div>',
+	);
+
+	comment_form( array(
+		'comment_field' => '<div class="form-group comment-form-comment">
+				<label for="comment">' . _x( 'Comment', 'noun' ) . '</label>
+				<textarea class="form-control" id="comment" name="comment" rows="8"></textarea>
+			</div>',
+		'class_submit' => 'btn btn-default submit',
+		'fields' => apply_filters( 'comment_form_default_fields', $fields ),
+		'label_submit' => __( 'Send' ),
 	) );
-	
-	the_comments_navigation();
-	
+
 	?>
-<?php endif; ?>
-<?php if ( ! comments_open() ): ?>
-	<p><?php _e( 'Comments are closed.', 'jabonnici' ); ?></p>
-<?php endif; ?>
-<?php
-
-$fields = array(
-	'author' => '<div class="form-group comment-form-author">
-			<label for="author">' . __( 'Name', 'domainreference' ) . '</label> ' .
-			( $req ? '<span class="required label label-default">*</span>' : '' ) .
-			'<input class="form-control" id="author" name="author" type="text" value="' .
-			esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . '>
-		</div>',
-	'email' => '<div class="form-group comment-form-email"><label for="email">' . __( 'Email', 'domainreference' ) . '</label> ' .
-			( $req ? '<span class="required label label-default">*</span>' : '' ) . ' (' . __( 'Will be kept anonymous.' ) . ')
-			<input class="form-control" id="email" name="email" type="email" value="' . esc_attr(  $commenter['comment_author_email'] ) .
-			'" size="30"' . $aria_req . ' />
-		</div>',
-	'url' => '<div class="form-group comment-form-url">
-				<label for="url">' . __( 'Website', 'domainreference' ) . '</label>' .
-				'<input class="form-control" id="url" name="url" type="url" value="' . esc_attr( $commenter['comment_author_url'] ) .
-				'" size="30" />
-		</div>',
-);
-
-comment_form( array(
-	'comment_field' => '<div class="form-group comment-form-comment">
-			<label for="comment">' . _x( 'Comment', 'noun' ) . '</label>
-			<textarea class="form-control" id="comment" name="comment" rows="8"></textarea>
-		</div>',
-	'class_submit' => 'btn btn-default submit',
-	'fields' => apply_filters( 'comment_form_default_fields', $fields ),
-	'label_submit' => __( 'Send' ),
-) );
-
-?>
+	</div>
 </div>
--- single.php
+++ single.php
@@ -5,7 +5,6 @@ get_header();
 the_post();
 
 ?>
-
 <div class="page-header">
 	<h1><?php the_title(); ?></h1>
 </div>
@@ -14,15 +13,8 @@ the_post();
 <?php if ( is_user_logged_in() && current_user_can( 'edit_posts' ) ): ?>
 	<a class="btn btn-default" href="<?php echo admin_url(); ?>post.php?post=<?php the_ID(); ?>&action=edit" role="button">Edit</a>
 <?php endif; ?>
-<p>Filed under:</p>
-<?php the_category(); ?>
-<article <?php post_class(); ?>>
-<?php
-
-the_content();
-
-?>
-</article>
+<p>Filed under: <?php the_category( ' ' ); ?></p>
+<article <?php post_class(); ?>><?php the_content(); ?></article>
 <nav>
 	<ul class="pager">
 		<li class="previous"><?php previous_post_link( '%link', '<span aria-hidden="true">&larr;</span> %title' ); ?></li>
