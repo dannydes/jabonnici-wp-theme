8c9b59757ff73ef342a6ffaaaddd09d82288c6d6
Daniel Desira <desiradaniel2007@gmail.com>
2016-03-01 15:29:27 +0100

    author bio

--- single.php
+++ single.php
@@ -9,12 +9,13 @@ the_post();
 	<h1><?php the_title(); ?></h1>
 </div>
 <time><?php the_time( get_option( 'date_format' ) ); ?></time>
-<?php echo the_author_link(); ?>
 <?php if ( is_user_logged_in() && current_user_can( 'edit_posts' ) ): ?>
 	<a class="btn btn-default" href="<?php echo admin_url(); ?>post.php?post=<?php the_ID(); ?>&action=edit" role="button">Edit</a>
 <?php endif; ?>
 <p>Filed under: <?php the_category( ' ' ); ?></p>
 <article <?php post_class(); ?>><?php the_content(); ?></article>
+<h2>About the author: <?php the_author_link(); ?></h2>
+<?php the_author_description(); ?>
 <nav>
 	<ul class="pager">
 		<li class="previous"><?php previous_post_link( '%link', '<span aria-hidden="true">&larr;</span> %title' ); ?></li>
