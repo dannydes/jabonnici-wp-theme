bc8ddeda3862c547890d5640da018ee64e6497b7
Daniel Desira <desiradaniel2007@gmail.com>
2016-03-09 12:19:31 +0100

    navigation breadcrumbs for subpages

--- page.php
+++ page.php
@@ -4,7 +4,17 @@ get_header();
 
 the_post();
 
+$page_ancestors = get_post_ancestors( get_the_ID() );
+
+if ( ! empty( $page_ancestors ) ):
 ?>
+<ol class="breadcrumb">
+	<?php foreach ( $page_ancestors as $page_id ): ?>
+	<li><a href="<?php the_permalink( $page_id ); ?>"><?php echo esc_html( get_the_title( $page_id ) ); ?></a></li>
+	<?php endforeach; ?>
+	<li class="active"><?php the_title(); ?></li>
+</ol>
+<?php endif; ?>
 <div class="page-header">
 	<h1><?php the_title(); ?></h1>
 </div>
